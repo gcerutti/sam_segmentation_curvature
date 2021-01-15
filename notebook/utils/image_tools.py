2dacbb86e906461918041f0b7e63d342eb612706
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2021-01-15 14:50:57 +0100

    use mersitem mesh in seg_curvature notebook

--- notebook/utils/image_tools.py
+++ notebook/utils/image_tools.py
@@ -2,8 +2,42 @@ import numpy as np
 import scipy.ndimage as nd
 import pandas as pd
 
+from timagetk.components import SpatialImage, LabelledImage
+from timagetk.plugins import h_transform, region_labeling, segmentation
+
 from cellcomplex.utils import array_dict
 
+def membrane_image_segmentation(img, gaussian_sigma=0.75, h_min=None, segmentation_gaussian_sigma=0.5, volume_threshold=None):
+
+    voxelsize = np.array(img.voxelsize)
+
+    if h_min is None:
+        h_min = 2 if img.dtype==np.uint8 else 1000
+
+    smooth_image = nd.gaussian_filter(img, sigma=gaussian_sigma / voxelsize).astype(img.dtype)
+    smooth_img = SpatialImage(smooth_image, voxelsize=voxelsize)
+
+    ext_img = h_transform(smooth_img, h=h_min, method='min')
+
+    seed_img = region_labeling(ext_img, low_threshold=1, high_threshold=h_min, method='connected_components')
+
+    seg_smooth_image = nd.gaussian_filter(img, sigma=segmentation_gaussian_sigma / voxelsize).astype(img.dtype)
+    seg_smooth_img = SpatialImage(seg_smooth_image, voxelsize=voxelsize)
+
+    seg_img = segmentation(seg_smooth_img, seed_img, control='first', method='seeded_watershed')
+
+    if volume_threshold is not None:
+        seg_volumes = dict(zip(np.arange(seg_img.max()) + 1,
+                               nd.sum(np.prod(voxelsize) * np.ones_like(seg_img),
+                                      seg_img,
+                                      index=np.arange(seg_img.max()) + 1)))
+
+        labels_to_remove = np.array(list(seg_volumes.keys()))[np.array(list(seg_volumes.values())) > volume_threshold]
+        print("--> Removing too large labels :", labels_to_remove)
+        for l in labels_to_remove:
+            seg_img[seg_img == l] = 1
+
+    return seg_img
 
 def labelled_image_projection(seg_img, axis=2, direction=1, background_label=1, return_coords=False):
     """Compute the 2D projection of a labelled image along the specified  axis
--- notebook/utils/property_image_tools.py
+++ notebook/utils/property_image_tools.py
@@ -1,3 +1,5 @@
+from multiprocessing import Pool
+
 import numpy as np
 import scipy.ndimage as nd
 import pandas as pd
@@ -36,7 +38,42 @@ def image_property_morphological_operation(p_img, property_name, method='erosion
             morpho_property *= p_img.image_property('layer').values(labels) == layer_restriction
         p_img.update_image_property(property_name,dict(zip(labels,morpho_property)))
 
-        
+
+def _max_count(all_values):
+    values = np.unique(all_values)
+    count = nd.sum(np.ones_like(all_values), all_values, index=values)
+    if (count == np.max(count)).sum() == 1:
+        return values[np.argmax(count)]
+    else:
+        max_values = values[count == np.max(count)]
+        if all_values[0] in max_values:
+            return all_values[0]
+        else:
+            return max_values[0]
+
+
+def image_property_median_filtering(p_img, property_name, labels=None, mode='label', iterations=1):
+    if labels is None:
+        labels = list(p_img.labels)
+
+    label_neighbors = [[n for n in p_img._image_graph.neighbors(c) if n in labels] for c in labels]
+
+    for iteration in range(iterations):
+        label_properties = p_img.image_property(property_name).values(labels)
+        label_neighbor_properties = [p_img.image_property(property_name).values(n) for n in label_neighbors]
+        label_neighborhood_properties = [[p] + list(n_p) for p,n_p in zip(label_properties,label_neighbor_properties)]
+
+        pool = Pool()
+        if mode == 'label':
+            median_neighborhood_properties = pool.map(_max_count,label_neighborhood_properties)
+        elif mode == 'scalar':
+            median_neighborhood_properties = pool.map(np.median,label_neighborhood_properties)
+        else:
+            median_neighborhood_properties = label_properties
+
+        p_img.update_image_property(property_name,dict(zip(labels,median_neighborhood_properties)))
+
+
 def property_image_to_dataframe(p_img, labels=None):
 
     cell_labels = list(p_img.labels)
